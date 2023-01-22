73bea682aa78830aa06840e834c29f2469949389
bagasrr <bagasramadhan239@gmail.com>
2023-01-22 19:03:25 +0700

    Carousel Tenplate

---
+++ index.js
@@ -0,0 +1,10 @@
+const goRight = () => {
+  document.querySelector(".carousel").scrollLeft += 1000;
+};
+const goLeft = () => {
+  document.querySelector(".carousel").scrollLeft -= 1000;
+};
+
+const slider = () => {
+  let carouselSlider = document.querySelector(".carousel");
+};
